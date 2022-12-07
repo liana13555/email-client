16ba6b518a808610c09ae5b550687be4b72fa90a
liana13555 <barishovaliana@gmail.com>
2022-12-07 13:32:09 +0100

    Updated components

--- emailclient/src/app/auth/signup/signup.component.ts
+++ emailclient/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatchPassword } from "../validators/match-password";
+import { UniqueUsername } from "../validators/unique-username";
+import { AuthService } from "../auth.service";
 
 @Component({
   selector: 'app-signup',
@@ -9,12 +11,14 @@ import { MatchPassword } from "../validators/match-password";
 })
 export class SignupComponent implements OnInit {
   authForm = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(20),
-      Validators.pattern(/^[a-z0-9]+$/)
-    ]
+    username: new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+        Validators.pattern(/^[a-z0-9]+$/)
+      ],
+      [this.uniqueUsername.validate]
     ),
     password: new FormControl('', [
       Validators.required,
@@ -29,9 +33,23 @@ export class SignupComponent implements OnInit {
   }, { validators: [this.matchPassword.validate] }
   )
 
-  constructor(private matchPassword: MatchPassword) { }
+  constructor(
+    private matchPassword: MatchPassword,
+    private uniqueUsername: UniqueUsername,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.authForm.invalid) {
+      return
+    }
+
+    this.authService.signup(this.authForm.value).subscribe(response => {
+      console.log(response);      
+    })
   }
 
 }
