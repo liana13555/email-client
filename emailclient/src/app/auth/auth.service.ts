ad24539b0c4ce7e7db2061c323ef93e58176fabd
liana13555 <barishovaliana@gmail.com>
2022-12-08 14:06:13 +0100

    Added Sign In Component

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -22,6 +22,11 @@ interface SignedinResponse {
   username: string
 }
 
+interface SigninCredentials {
+  username:  string,
+  password: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -62,5 +67,14 @@ export class AuthService {
         })
 
     )
+  }
+
+  signin(credentials: any) {
+    return this.http.post<SignupResponse>(`${this.rootUrl}/auth/signin`, credentials)
+      .pipe(
+        tap(() => {
+          this.signedin$.next(true)
+        })
+      )
   }
 }
