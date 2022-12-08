38bb5da581f7c35207b17eb3c5906464403ae7b7
liana13555 <barishovaliana@gmail.com>
2022-12-08 09:08:52 +0100

    Added interfaces

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -5,20 +5,31 @@ interface usernameAvailableResponse {
   available: boolean
 }
 
+interface SignupCredentials {
+  username:  string,
+  password: string,
+  passwordConfirmation: string
+}
+
+interface SignupResponse {
+  username: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  rootUrl = 'https://api.angular-email.com'
 
   constructor(private http: HttpClient) { }
 
   usernameAvailable(username: string) {
-    return this.http.post<usernameAvailableResponse>('https://api.angular-email.com/auth/username', {
+    return this.http.post<usernameAvailableResponse>(`${this.rootUrl}/auth/username`, {
       username: username
     })
   }
 
   signup(credentials: any) {
-    return this.http.post<any>('https://api.angular-email.com/auth/signup', credentials)
+    return this.http.post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials)
   }
 }
