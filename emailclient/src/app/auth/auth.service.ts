a592cb85230c583c95405db6548cb79560d4100e
liana13555 <barishovaliana@gmail.com>
2022-12-08 13:16:37 +0100

    Added Sign Out

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -17,6 +17,11 @@ interface SignupResponse {
   username: string
 }
 
+interface SignedinResponse {
+  authenticated: boolean,
+  username: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -42,10 +47,20 @@ export class AuthService {
   }
 
   checkAuth() {
-    return this.http.get(`${this.rootUrl}/auth/signedin`).pipe(
-      tap(response=> {
-        console.log(response);        
+    return this.http.get<SignedinResponse>(`${this.rootUrl}/auth/signedin`).pipe(
+      tap(({ authenticated }) => {
+         this.signedin$.next(authenticated)      
       }) 
+    )
+  }
+
+  signout() {
+    return this.http.post(`${this.rootUrl}/auth/signout`, {})
+      .pipe(
+        tap(() => {
+          this.signedin$.next(false)
+        })
+
     )
   }
 }
