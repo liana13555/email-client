7987384446e9ab76af5dcc98dfff3dc8119530a6
liana13555 <barishovaliana@gmail.com>
2022-12-08 12:13:35 +0100

    Update auth-http-interceptor

--- emailclient/src/app/auth/auth-http-interceptor.ts
+++ emailclient/src/app/auth/auth-http-interceptor.ts
@@ -10,19 +10,22 @@ export class AuthHttpInterceptor implements HttpInterceptor{
         const modifiedReq = req.clone({
             withCredentials: true
         })
-        return next.handle(modifiedReq).pipe(
-            filter(val => val.type === HttpEventType.Sent),
-            tap(val => {
-                console.log('Sent a request');
+        return next.handle(modifiedReq)
+        // return next.handle(modifiedReq).pipe(
+        //     filter(val => val.type === HttpEventType.Response),
+        //     tap(val => {
+        //         console.log('Sent a request');
+        //         console.log(val);
                 
-                // if (val.type === HttpEventType.Sent) {
-                //     console.log('Request was sent to server');                    
-                // }   
                 
-                // if (val.type === HttpEventType.Response) {
-                //     console.log('Got a response from the API', val);                    
-                // }                     
-            })
-        )
+        //         // if (val.type === HttpEventType.Sent) {
+        //         //     console.log('Request was sent to server');                    
+        //         // }   
+                
+        //         // if (val.type === HttpEventType.Response) {
+        //         //     console.log('Got a response from the API', val);                    
+        //         // }                     
+        //     })
+        // )
     }
 }
