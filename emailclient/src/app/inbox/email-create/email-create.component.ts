16bce4c1c04830c262fc776b3cb8c22332214d17
liana13555 <barishovaliana@gmail.com>
2022-12-13 10:35:11 +0100

    Added components

---
+++ emailclient/src/app/inbox/email-create/email-create.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Email } from '../email';
+
+@Component({
+  selector: 'app-email-create',
+  templateUrl: './email-create.component.html',
+  styleUrls: ['./email-create.component.css']
+})
+export class EmailCreateComponent implements OnInit {
+  showModal = false 
+  email: Email
+
+  constructor() {
+    this.email = {
+      id: '',
+      to: '',
+      subject: '',
+      html: '',
+      text: '',
+      from: 'test11@angular-email.com'
+    }
+
+   }
+
+  ngOnInit(): void {
+  }
+}
